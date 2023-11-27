2db3ea5e2fedf189552abc444e343a5a36b044cd
angie-daza <angiedaza11@gmail.com>
2023-11-27 18:06:46 -0500

    Commut de ejemplo 3

---
+++ ej-3/back.js
@@ -0,0 +1,55 @@
+// back.js
+
+function calculate() {
+    // Obtener los datos del formulario
+
+    var mes = document.getElementById("month").value;
+    //var num2 = parseFloat(document.getElementById("num2").val
+    console.log(mes);
+
+    // Realizar la operación
+    var result;
+    switch (mes) {
+        case "uno":
+            result = "Enero";
+            break;
+        case "dos":
+            result = "Febrero";
+            break;
+        case "tres":
+            result = "Marzo";
+            break;
+        case "cuatro":
+            result = "Abril";
+            break;
+        case "cinco":
+            result = "Mayo";
+            break;
+        case "seis":
+            result = "Junio";
+            break;
+        case "siete":
+            result = "Julio";
+            break;
+        case "ocho":
+            result = "Agosto";
+            break;
+         case "nueve":
+            result = "Septiembre";
+            break;
+        case "diez":
+            result = "Octubre";
+            break;
+        case "once":
+            result = "Noviembre";
+            break;
+        case "doce":
+            result = "Diciembre";
+            break;
+        default:
+            result = "Operación no válida";
+    }
+
+    // Mostrar el resultado en la página
+    document.getElementById("result").innerText = "Resultado: " + result;
+}
